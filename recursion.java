9f89df88c1d801b0fa7df6eb3d9a4072194e06f0
Yongho Lee <ylee01@stuy.edu>
2019-02-01 10:54:15 -0500

    Added recursion.java

---
+++ recursion.java
@@ -0,0 +1,27 @@
+public class recursion{
+    /*You may write additional private methods */
+
+    /*Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
+
+    */
+    public static double sqrt(double n, double tolerance){
+
+    }
+
+    /*Recursively find the n'th fibbonaci number in linear time
+     *fib(0) = 0
+     *fib(1) = 1
+     *fib(5) = 5
+     *precondition: n is non-negative
+     */
+    public static int fib(int n){
+
+    }
+
+    /*As Per classwork*/
+    public static ArrayList<Integer> makeAllSums(){
+    }
+
+}
