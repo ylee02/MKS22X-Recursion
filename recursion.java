b96df6321e58c760abb65a3264d69b7c13c25d5b
ylee02 <ylee01@stuy.edu>
2019-03-28 08:16:54 +0900

    Fixed sqrt, probably works now

--- recursion.java
+++ recursion.java
@@ -1,5 +1,65 @@
 import java.util.ArrayList;
 public class recursion{
+	public static void main(String[] args) {
+        System.out.println("sqrt()");
+        System.out.println("-----------------------\n");
+
+        System.out.println("sqrt(4, 0.00001)");
+        System.out.println("EXPECTED: 2");
+        System.out.println(sqrt(4, 0.00001));         // 2
+
+        System.out.println("----");
+
+        System.out.println("sqrt(8, 0.00001)");
+        System.out.println("EXPECTED: ~2.8284");
+        System.out.println(sqrt(8, 0.00001));         // ~2.8284
+
+        System.out.println("----");
+
+        System.out.println("sqrt(0, 0.00001)");
+        System.out.println("EXPECTED: 0");
+        System.out.println(sqrt(0, 0.00001));         // 0
+
+        System.out.println("\n");
+
+        System.out.println("fib()");
+        System.out.println("-----------------------\n");
+
+        System.out.println("fib(0)");
+        System.out.println("EXPECTED: 0");
+        System.out.println(fib(0));                   // 0
+
+        System.out.println("----");
+
+        System.out.println("fib(1)");
+        System.out.println("EXPECTED: 1");
+        System.out.println(fib(1));                   // 1
+
+        System.out.println("----");
+
+        System.out.println("fib(7)");
+        System.out.println("EXPECTED: 13");
+        System.out.println(fib(7));                   // 13
+
+        System.out.println("makeAllSums()");
+        System.out.println("-----------------------\n");
+
+        System.out.println("makeAllSums(0)");
+        System.out.println("EXPECTED: [0]");
+        System.out.println(makeAllSums(0));           // [0]
+
+        System.out.println("----");
+
+        System.out.println("makeAllSums(1)");
+        System.out.println("EXPECTED: [1, 0]");
+        System.out.println(makeAllSums(1));           // [1, 0]
+
+        System.out.println("----");
+
+        System.out.println("makeAllSums(3)");
+        System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+        System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
+}
     /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
@@ -12,7 +72,7 @@ public class recursion{
 
     }
     public static double sqrth(double n, double tol, double guess) {
-      if (Math.abs(guess * guess - n) / n <= tol){
+      if (Math.abs(guess * guess - n) / n <= tol || guess == 0){
         return guess;
       }
       return sqrth(n, tol, (n / guess + guess) / 2);
